1d37ece883c2be700b8b4aae7d29e8c9636f6a66
Jason Tesreau <jasontesreau@Jasons-MacBook-Pro-2.local>
2019-02-05 17:05:14 -0600

    Implemented actions

---
+++ Hello World/HelloWorldViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HelloWorldViewController.swift
+//  Hello World
+//
+//  Created by Jason Tesreau on 2/5/19.
+//  Copyright © 2019 Jason Tesreau. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doHello(_ sender: UIButton) {
+        messageLabel.text = "Hello World!"
+    }
+    
+    @IBAction func doClear(_ sender: UIButton) {
+        messageLabel.text = ""
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
